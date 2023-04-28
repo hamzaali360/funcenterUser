5a7b9b9914fb128ba43ac0b0d4518893193a466b
hamzaali360 <hamali1708@gmail.com>
2023-04-27 23:45:56 -0500

    finish project

--- app/src/main/java/com/cs478/funcenteruser/MainActivity.java
+++ app/src/main/java/com/cs478/funcenteruser/MainActivity.java
@@ -1,14 +1,273 @@
 package com.cs478.funcenteruser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.core.app.ActivityCompat;
+
+import com.cs478.KeyComon.FunCenterService;
+
+public class MainActivity extends Activity {
+    protected static final String TAG = "MainActivity";
+    protected static final int PERMISSION_REQUEST = 0;
+    private FunCenterService funCenterServices;
+    private boolean mIsBound = false;
+    private boolean isPlaying = false;
+    private Button song1;
+    private Button song2;
+    private Button song3;
+    private Button picture1;
+    private Button picture2;
+    private Button picture3;
+    private Button stop;
+    private ImageButton playAndPause;
+    private ImageView picture;
+    private int curID;
+    private boolean stopPressed = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        song1 = findViewById(R.id.song1);
+        song2 = findViewById(R.id.song2);
+        song3 = findViewById(R.id.song3);
+        picture1 = findViewById(R.id.picture1);
+        picture2 = findViewById(R.id.picture2);
+        picture3 = findViewById(R.id.picture3);
+        playAndPause = findViewById(R.id.playAndPause);
+        stop = findViewById(R.id.stop);
+        picture = findViewById(R.id.picture);
+
+        song1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSong(0);
+            }
+        });
+
+        picture1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImage(0);
+            }
+        });
+
+        song2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSong(1);
+            }
+        });
+
+        picture2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImage(1);
+            }
+        });
+
+        song3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSong(2);
+            }
+        });
+
+        picture3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImage(2);
+            }
+        });
+
+        playAndPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if(isPlaying) {
+                        funCenterServices.pauseAudio();
+                        playAndPause.setImageResource(android.R.drawable.ic_media_play);
+                        isPlaying = false;
+                    }
+                    else {
+                        if(!stopPressed){
+                            funCenterServices.playAudio(curID);
+                            playAndPause.setImageResource(android.R.drawable.ic_media_pause);
+                            isPlaying = true;
+                        }
+
+                    }
+                } catch (RemoteException e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    funCenterServices.stopAudio();
+                    stopPressed = true;
+                    playAndPause.setImageResource(android.R.drawable.ic_media_play);
+                    isPlaying = false;
+                } catch (RemoteException e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        });
+
+    }
+
+    private void playSong(int id) {
+        try {
+            if(mIsBound) {
+                if(!isPlaying && stopPressed) {
+                    funCenterServices.playAudio(id);
+                    isPlaying = true;
+                    playAndPause.setImageResource(android.R.drawable.ic_media_pause);
+                    curID = id;
+                    stopPressed = false;
+                }
+
+            }
+            else {
+                Log.i(TAG, "Ugo says that the service was not bound!");
+            }
+        } catch (RemoteException e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    private void showImage(int id) {
+        try {
+            if (mIsBound) {
+                picture.setImageBitmap(funCenterServices.getPicture(id));
+            }
+            else {
+                Log.i(TAG, "Ugo says that the service was not bound!");
+            }
+        } catch (RemoteException e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (checkSelfPermission("com.cs478.funcenter.GEN_ID") != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{"com.cs478.funcenter.GEN_ID"}, PERMISSION_REQUEST);
+        }
+        else {
+            checkBindingAndBind();
+        }
+    }
+
+    protected void checkBindingAndBind() {
+        if (!mIsBound) {
+
+            boolean b ;
+            Intent i = new Intent(FunCenterService.class.getName());
+            // here is where we get the info.
+            Log.e("Key client", i.toString()) ;
+
+            // UB:  Stoooopid Android API-21 no longer supports implicit intents
+            // to bind to a service #@%^!@..&**!@
+            // Must make intent explicit or lower target API level to 20.
+            ResolveInfo info = getPackageManager().resolveService(i, PackageManager.MATCH_ALL);
+            i.setComponent(new ComponentName(info.serviceInfo.packageName, info.serviceInfo.name));
+
+            b = bindService(i, this.mConnection, Context.BIND_AUTO_CREATE);
+            if (b) {
+                Log.i(TAG, "Ugo says bindService() succeeded!");
+            } else {
+                Log.i(TAG, "Ugo says bindService() failed!");
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST: {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // Permission granted, go ahead and bind to service
+                    checkBindingAndBind();
+                }
+                else {
+                    Toast.makeText(this, "BUMMER: No Permission :-(", Toast.LENGTH_LONG).show() ;
+                }
+            }
+            default: {
+                // do nothing
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+
+        if (mIsBound) {
+            unbindService(this.mConnection);
+        }
+    }
+
+    private final ServiceConnection mConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className, IBinder iservice) {
+
+            funCenterServices = FunCenterService.Stub.asInterface(iservice);
+            mIsBound = true;
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            try {
+                funCenterServices.stopAudio();
+            } catch (RemoteException e) {
+
+            }
+
+            funCenterServices = null;
+            mIsBound = false;
+
+
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            funCenterServices.stopAudio();
+        } catch (RemoteException e) {
+
+        }
     }
 }
